491a0eb7bf07d49b120d19f075c26e893934a885
Guruprasanna02 <guruprasannavasudevan02@gmail.com>
2022-07-27 21:08:42 +0530

    add: server-setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,12 @@ python server.py
 
 #Since mysql container takes time to start completely, we need to wait for few seconds
 
-# python setupdb.py
+until nc -z -v -w30 db 3306
+do
+  echo "Waiting for database connection..."
+  # wait for 5 seconds before check again
+  sleep 5
+done
+
+python setupdb.py
 
